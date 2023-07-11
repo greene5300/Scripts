a87ac3f647f57d00d9dc29a801a1313fb2dee244
greene5300 <91769644+greene5300@users.noreply.github.com>
2023-07-11 14:47:47 -0500

    Update Remove_Widgets.ps1

--- Remove_Widgets.ps1
+++ Remove_Widgets.ps1
@@ -1,1 +1,2 @@
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -type "Dword" -Value "0"
+Remove-Item "C:\\Synchro\resources\github\git\scripting*"-Force -Recurse
